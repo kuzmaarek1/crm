730433263e0a59e4c7dd6792ee17a7d708cedab5
arm650 <arm650>
2022-12-21 23:03:46 +0100

    Responsive navbar

---
+++ crm_react/src/components/Navbar/MobileNavbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import * as Styles from "./styles";
+
+const MobileNavbar = ({ auth, teams, setShowNabar, showNavbar }) => {
+  if (auth)
+    return (
+      <Styles.MobileNavbar>
+        <Styles.HaburgerLines
+          onClick={() => setShowNabar((prevState) => !prevState)}
+        >
+          {[...Array(3).keys()].map((key) => (
+            <Styles.HaburgerLine
+              showNavbar={showNavbar}
+              number={key + 1}
+              key={key}
+            ></Styles.HaburgerLine>
+          ))}
+        </Styles.HaburgerLines>
+
+        <Styles.MobileUl showNavbar={!showNavbar}>
+          {teams.currentTeam &&
+            ["leads", "teams", "clients"].map((props, index) => (
+              <Styles.MobileLink
+                onClick={() => setShowNabar(false)}
+                to={props}
+                key={index}
+              >
+                {props[0].toUpperCase()}
+                {props.slice(1)}
+              </Styles.MobileLink>
+            ))}
+          <Styles.MobileLink
+            onClick={() => setShowNabar(false)}
+            to="my-account"
+          >
+            My account{" "}
+          </Styles.MobileLink>
+        </Styles.MobileUl>
+      </Styles.MobileNavbar>
+    );
+};
+
+export default MobileNavbar;
--- crm_react/src/components/Navbar/index.jsx
+++ crm_react/src/components/Navbar/index.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { teamsApiSlice } from "reducers/teamsApiSlice";
+import MobileNavbar from "./MobileNavbar";
 import * as Styles from "./styles";
 
 const Navbar = () => {
+  const [showNavbar, setShowNabar] = useState(false);
   const auth = useSelector((state) => state.auth.authData);
   const teams = useSelector((state) => state.teams);
   const dispatch = useDispatch();
@@ -20,25 +22,33 @@ const Navbar = () => {
 
   return (
     <Styles.Wrapper>
-      <Styles.Title to="/teams">
-        {teams.currentTeam && auth
-          ? teams.currentTeam.name.length > 15
-            ? `${teams.currentTeam.name.substring(0, 15)}...`
-            : teams.currentTeam.name
-          : "CRM"}
-      </Styles.Title>
+      <Styles.TitleWrapper>
+        <Styles.Title to="/teams">
+          {teams.currentTeam && auth
+            ? teams.currentTeam.name.length > 30
+              ? `${teams.currentTeam.name.substring(0, 30)}...`
+              : teams.currentTeam.name
+            : "CRM"}
+        </Styles.Title>
+      </Styles.TitleWrapper>
       {auth && (
-        <Styles.Links>
-          {teams.currentTeam && (
-            <>
-              <Styles.Link to="leads">Leads </Styles.Link>
-              <Styles.Link to="teams">Teams </Styles.Link>
-              <Styles.Link to="clients">Clients </Styles.Link>
-            </>
-          )}
+        <Styles.Ul>
+          {teams.currentTeam &&
+            ["leads", "teams", "clients"].map((props, index) => (
+              <Styles.Link key={index} to={props}>
+                {props[0].toUpperCase()}
+                {props.slice(1)}
+              </Styles.Link>
+            ))}
           <Styles.Link to="my-account">My account </Styles.Link>
-        </Styles.Links>
+        </Styles.Ul>
       )}
+      <MobileNavbar
+        auth={auth}
+        teams={teams}
+        setShowNabar={setShowNabar}
+        showNavbar={showNavbar}
+      />
     </Styles.Wrapper>
   );
 };
