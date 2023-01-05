78f87bd7647501ce5e3a0de5e811d3d11f645848
arm650 <arm650>
2023-01-05 15:42:25 +0100

    Improve code - testing

--- crm_react/src/views/Authenticated/LoginPage/index.test.jsx
+++ crm_react/src/views/Authenticated/LoginPage/index.test.jsx
@@ -16,25 +16,26 @@ describe("Render login page", () => {
   });
 });
 
+const handleChangePage = () => {
+  const switchButton = screen.getByRole("button", { name: /switch-button/i });
+  fireEvent.click(switchButton);
+};
 describe("Render signup page", () => {
   test("Render elements button and header on signup page", () => {
     render(<LoginPage />);
-    const switchButton = screen.getByRole("button", { name: /switch-button/i });
-    fireEvent.click(switchButton);
+    handleChangePage();
     const signUpElements = screen.getAllByText(/sign up/i);
     expect(signUpElements).toHaveLength(2);
   });
   test("Not render elements with login page", () => {
     render(<LoginPage />);
-    const switchButton = screen.getByRole("button", { name: /switch-button/i });
-    fireEvent.click(switchButton);
+    handleChangePage();
     const signUpElements = screen.queryByText(/log in/i);
     expect(signUpElements).not.toBeInTheDocument();
   });
   test("Render input first name on login page", () => {
     render(<LoginPage />);
-    const switchButton = screen.getByRole("button", { name: /switch-button/i });
-    fireEvent.click(switchButton);
+    handleChangePage();
     const labelElements = screen.queryByLabelText(/first name/i);
     expect(labelElements).toBeInTheDocument();
   });
--- crm_react/src/views/Root.test.jsx
+++ crm_react/src/views/Root.test.jsx
@@ -2,22 +2,36 @@ import React from "react";
 import { render, screen, fireEvent, waitFor } from "test-utils";
 import { Root } from "views";
 
+const handleChangeInputsAuth = (username, password) => {
+  [username, password].forEach((items, index) => {
+    const label =
+      index === 0
+        ? screen.getByLabelText(/username/i)
+        : screen.getByLabelText(/password/i);
+    fireEvent.change(label, {
+      target: { value: items },
+    });
+  });
+};
+
 describe("Login and Logout", () => {
+  test("Login with incorrect username, password", async () => {
+    render(<Root />);
+    handleChangeInputsAuth("akuzma5@gmail.com", "Mrooodyle1eee@");
+    fireEvent.click(screen.getByRole("button", { name: /login-or-signup/i }));
+    const errorElement = await screen.findByText(
+      /your username or password is incorrect/i
+    );
+    expect(errorElement).toBeInTheDocument();
+  });
+
   test("Login with correct username, password", async () => {
     render(<Root />);
-    const labelUsername = screen.getByLabelText(/username/i);
-    const username = "akuzma555@gmail.com";
-    const labelPassword = screen.getByLabelText(/password/i);
-    const password = "Mrooodyle1eee@";
-    fireEvent.change(labelUsername, {
-      target: { value: username },
-    });
-    fireEvent.change(labelPassword, {
-      target: { value: password },
-    });
+    handleChangeInputsAuth("akuzma555@gmail.com", "Mrooodyle1eee@");
     fireEvent.click(screen.getByRole("button", { name: /login-or-signup/i }));
     await waitFor(() => screen.findByText(/dawid/i));
   });
+
   test("Logout", async () => {
     render(<Root />);
     const buttonLogout = screen.getByRole("button", {
@@ -27,23 +41,4 @@ describe("Login and Logout", () => {
     const logInElements = await screen.findAllByText(/log in/i);
     expect(logInElements).toHaveLength(2);
   });
-});
-
-test("Login with incorrect username, password", async () => {
-  render(<Root />);
-  const labelUsername = screen.getByLabelText(/username/i);
-  const username = "akuzma5@gmail.com";
-  const labelPassword = screen.getByLabelText(/password/i);
-  const password = "Mrooodyle1eee@";
-  fireEvent.change(labelUsername, {
-    target: { value: username },
-  });
-  fireEvent.change(labelPassword, {
-    target: { value: password },
-  });
-  fireEvent.click(screen.getByRole("button", { name: /login-or-signup/i }));
-  const errorElement = await screen.findByText(
-    /your username or password is incorrect/i
-  );
-  expect(errorElement).toBeInTheDocument();
 });
