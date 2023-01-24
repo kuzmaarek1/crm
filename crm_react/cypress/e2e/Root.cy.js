fd35b2fad2f483ceac5270b570fb1132a23b5cd9
arm650 <arm650>
2023-01-24 18:25:50 +0100

    Cypress - display lead

--- crm_react/cypress/e2e/Root.cy.js
+++ crm_react/cypress/e2e/Root.cy.js
@@ -1,11 +1,16 @@
 import * as constants from "test/constants";
 
 describe("Login", () => {
-  it("Login with correct username, password", () => {
+  it("Login with correct username, password", async () => {
     cy.visit("http://localhost:3000/");
     constants.formDataLogin[1].forEach(({ name, value }) => {
       cy.findByLabelText(name).type(value);
     });
     cy.findByRole("button", { name: /login-or-signup/i }).click();
+    cy.findByText(/dawid/i);
+
+    cy.findByTestId("leads").click();
+    cy.findByTestId(/loading/i);
+    cy.findAllByTestId(/cell/i).should("have.length", 80);
   });
 });
