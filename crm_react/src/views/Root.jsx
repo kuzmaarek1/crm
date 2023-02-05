5df043996a4a138ae54a99a08a7aef8811a45a26
arm650 <arm650>
2023-02-05 13:39:02 +0100

    Refactor - conditions

--- crm_react/src/views/Root.jsx
+++ crm_react/src/views/Root.jsx
@@ -4,9 +4,7 @@ import { AuthenticatedApp, UnAuthenticatedApp } from "views";
 
 const Root = () => {
   const auth = useSelector((state) => state.auth.authData);
-  return (
-    <>{auth?.auth_token ? <AuthenticatedApp /> : <UnAuthenticatedApp />}</>
-  );
+  return auth?.auth_token ? <AuthenticatedApp /> : <UnAuthenticatedApp />;
 };
 
 export default Root;
